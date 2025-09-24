453655b322bec953fef7073b76d8193433eb1811
goraria <gorariagroup@gmail.com>
2025-09-24 16:59:23 +0700

    Japtor - Completed CRUD Inventory and Create Cart Components

--- applications/client/components/layout/navbar.tsx
+++ applications/client/components/layout/navbar.tsx
@@ -68,6 +68,8 @@ import { ClerkSignIn, ClerkSignUp } from "@/components/elements/clerk-buttons";
 import { SearchBarHolder } from "@/components/elements/search-bar";
 import { NavUser } from "@/components/dashboard/nav-user";
 import { navigation, appGlobal } from "@/constants/constants";
+import { CartButton } from "@/components/elements/cart";
+import { NotifyButton } from "@/components/elements/notify";
 
 export function Navbar() {
   const { user } = useUser();
@@ -314,70 +316,72 @@ export function Navbar() {
               </TooltipContent>
             </Tooltip> */}
             {/* Notifications */}
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button variant="ghost" size="icon" className="relative">
-                  <Bell className="h-6 w-6" />
-                  <span className="absolute -right-1 -top-1 flex h-4 w-4 items-center justify-center rounded-full bg-destructive text-[10px] text-white">
-                    8
-                  </span>
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent align="end" className="w-80">
-                <DropdownMenuLabel>Thông báo</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <div className="max-h-[320px] overflow-auto">
-                  <DropdownMenuItem className="flex flex-col items-start gap-1">
-                    <div className="flex items-center gap-2">
-                      <span className="font-medium">Tin nhắn mới</span>
-                      <span className="text-xs text-muted-foreground">1h ago</span>
-                    </div>
-                    <p className="text-sm text-muted-foreground">Bạn có tin nhắn mới từ Natalie</p>
-                  </DropdownMenuItem>
-                  <DropdownMenuItem className="flex flex-col items-start gap-1">
-                    <div className="flex items-center gap-2">
-                      <span className="font-medium">Tin nhắn mới</span>
-                      <span className="text-xs text-muted-foreground">1h ago</span>
-                    </div>
-                    <p className="text-sm text-muted-foreground">Bạn có tin nhắn mới từ Natalie</p>
-                  </DropdownMenuItem>
-                  {/* Add more notification items */}
-                </div>
-              </DropdownMenuContent>
-            </DropdownMenu>
+            <NotifyButton/>
+            {/*<DropdownMenu>*/}
+            {/*  <DropdownMenuTrigger asChild>*/}
+            {/*    <Button variant="ghost" size="icon" className="relative">*/}
+            {/*      <Bell className="h-6 w-6" />*/}
+            {/*      <span className="absolute -right-1 -top-1 flex h-4 w-4 items-center justify-center rounded-full bg-destructive text-[10px] text-white">*/}
+            {/*        8*/}
+            {/*      </span>*/}
+            {/*    </Button>*/}
+            {/*  </DropdownMenuTrigger>*/}
+            {/*  <DropdownMenuContent align="end" className="w-80">*/}
+            {/*    <DropdownMenuLabel>Thông báo</DropdownMenuLabel>*/}
+            {/*    <DropdownMenuSeparator />*/}
+            {/*    <div className="max-h-[320px] overflow-auto">*/}
+            {/*      <DropdownMenuItem className="flex flex-col items-start gap-1">*/}
+            {/*        <div className="flex items-center gap-2">*/}
+            {/*          <span className="font-medium">Tin nhắn mới</span>*/}
+            {/*          <span className="text-xs text-muted-foreground">1h ago</span>*/}
+            {/*        </div>*/}
+            {/*        <p className="text-sm text-muted-foreground">Bạn có tin nhắn mới từ Natalie</p>*/}
+            {/*      </DropdownMenuItem>*/}
+            {/*      <DropdownMenuItem className="flex flex-col items-start gap-1">*/}
+            {/*        <div className="flex items-center gap-2">*/}
+            {/*          <span className="font-medium">Tin nhắn mới</span>*/}
+            {/*          <span className="text-xs text-muted-foreground">1h ago</span>*/}
+            {/*        </div>*/}
+            {/*        <p className="text-sm text-muted-foreground">Bạn có tin nhắn mới từ Natalie</p>*/}
+            {/*      </DropdownMenuItem>*/}
+            {/*       Add more notification items */}
+            {/*    </div>*/}
+            {/*  </DropdownMenuContent>*/}
+            {/*</DropdownMenu>*/}
 
             {/* Carts */}
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button variant="ghost" size="icon" className="relative">
-                  <ShoppingCart className="h-6 w-6" />
-                  <span className="absolute -right-1 -top-1 flex h-4 w-4 items-center justify-center rounded-full bg-destructive text-[10px] text-white">
-                    {99}
-                  </span>
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent align="end" className="w-80">
-                <DropdownMenuLabel>Giỏ hàng</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <div className="max-h-[360px] overflow-auto">
-                  <DropdownMenuItem className="flex flex-col items-start gap-1">
-                    <div className="flex items-center gap-2">
-                      <span className="font-medium">New Message</span>
-                      <span className="text-xs text-muted-foreground">1h ago</span>
-                    </div>
-                    <p className="text-sm text-muted-foreground">You have new message from Natalie</p>
-                  </DropdownMenuItem>
-                  <DropdownMenuItem className="flex flex-col items-start gap-1">
-                    <div className="flex items-center gap-2">
-                      <span className="font-medium">New Message</span>
-                      <span className="text-xs text-muted-foreground">1h ago</span>
-                    </div>
-                    <p className="text-sm text-muted-foreground">You have new message from Natalie</p>
-                  </DropdownMenuItem>
-                  {/* Add more notification items */}
-                </div>
-              </DropdownMenuContent>
-            </DropdownMenu>
+            <CartButton/>
+            {/*<DropdownMenu>*/}
+            {/*  <DropdownMenuTrigger asChild>*/}
+            {/*    <Button variant="ghost" size="icon" className="relative">*/}
+            {/*      <ShoppingCart className="h-6 w-6" />*/}
+            {/*      <span className="absolute -right-1 -top-1 flex h-4 w-4 items-center justify-center rounded-full bg-destructive text-[10px] text-white">*/}
+            {/*        {99}*/}
+            {/*      </span>*/}
+            {/*    </Button>*/}
+            {/*  </DropdownMenuTrigger>*/}
+            {/*  <DropdownMenuContent align="end" className="w-80">*/}
+            {/*    <DropdownMenuLabel>Giỏ hàng</DropdownMenuLabel>*/}
+            {/*    <DropdownMenuSeparator />*/}
+            {/*    <div className="max-h-[360px] overflow-auto">*/}
+            {/*      <DropdownMenuItem className="flex flex-col items-start gap-1">*/}
+            {/*        <div className="flex items-center gap-2">*/}
+            {/*          <span className="font-medium">New Message</span>*/}
+            {/*          <span className="text-xs text-muted-foreground">1h ago</span>*/}
+            {/*        </div>*/}
+            {/*        <p className="text-sm text-muted-foreground">You have new message from Natalie</p>*/}
+            {/*      </DropdownMenuItem>*/}
+            {/*      <DropdownMenuItem className="flex flex-col items-start gap-1">*/}
+            {/*        <div className="flex items-center gap-2">*/}
+            {/*          <span className="font-medium">New Message</span>*/}
+            {/*          <span className="text-xs text-muted-foreground">1h ago</span>*/}
+            {/*        </div>*/}
+            {/*        <p className="text-sm text-muted-foreground">You have new message from Natalie</p>*/}
+            {/*      </DropdownMenuItem>*/}
+            {/*       Add more notification items */}
+            {/*    </div>*/}
+            {/*  </DropdownMenuContent>*/}
+            {/*</DropdownMenu>*/}
 
             {user ? (
               <>
